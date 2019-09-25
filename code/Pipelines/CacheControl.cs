1cb5f67eb75489798f9518b6cd0497d3bdc69532
Uladzimir Zaviaruha <zaverugov@gmail.com>
2019-09-25 09:49:09 +0100

    WIP

---
+++ code/Pipelines/CacheControl.cs
@@ -0,0 +1,12 @@
+﻿using Sitecore.Mvc.Pipelines.MvcEvents.ActionExecuted;
+
+namespace Feature.CDN.Pipelines
+{
+    public class CacheControl : ActionExecutedProcessor
+    {
+        public override void Process(ActionExecutedArgs args)
+        {
+            args.Context.RequestContext.HttpContext.Response.Cache.SetCacheability(System.Web.HttpCacheability.Public);
+        }
+    }
+}
