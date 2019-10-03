57c369449d31c0b223ced35a43d027b58d15181b
Ashar Shah <ashar_shah@epam.com>
2019-10-03 11:45:36 +0400

    Initial commit for not rendering static markup at all - instead of getting rid of rendered markup.

--- code/scripts/ApplyPatch.js
+++ code/scripts/ApplyPatch.js
@@ -7,12 +7,15 @@
 });
 
 function requestDynamicContent() {
+    var dynamicRenderingIds = getDynamicRenderingIds();
     var getDynamicContentUrl = window.location.href;
     getDynamicContentUrl = getDynamicContentUrl + (window.location.href.includes('?') ? "&" : "?");
     getDynamicContentUrl = getDynamicContentUrl + "GetDynamicContent=1";
 
     $.ajax({
         url: getDynamicContentUrl,
+        type: "GET",
+        beforeSend: function (xhr) { xhr.setRequestHeader('DynamicRenderingIds', dynamicRenderingIds.join("|")); },
         success: function (result) {
             injectDynamicContent(result);
         }
@@ -30,4 +33,13 @@ function injectDynamicContent(dynamicContent) {
             $(this).replaceWith(dynamicElement);
         }
     });
+}
+
+function getDynamicRenderingIds() {
+    var dynamicRenderingIds = [];
+    $('[data-rs="0"]').each(function() {
+        var uid = $(this).attr("data-rid");
+        dynamicRenderingIds.push(uid);
+    });
+    return dynamicRenderingIds;
 }
